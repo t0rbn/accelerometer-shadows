8130744827bda26098f31346bb268b367e556a75
Torben Reetz <torben.reetz@mindmatters.de>
2025-07-05 19:40:41 +0200

    add offset

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// document.querySelector('html').addEventListener('mousemove', e => {
-//     const mouseCoords = [e.clientX, e.clientY]
-//     const centerCoords = [window.innerWidth / 2, window.innerHeight / 2]
-//
-//
-//     const angle = (Math.atan2( mouseCoords[1] - centerCoords[1], mouseCoords[0] - centerCoords[0])  * 180) / Math.PI
-//     const norm = (450 + angle) % 360
-//
-//     document.querySelectorAll('.foo').forEach(x => x.style.setProperty('--shadow-direction', angle + 'deg'))
-//
-// })
-
-
 window.addEventListener("deviceorientation", (e) => {
-    document.querySelectorAll('.foo').forEach(x => x.style.setProperty('--shadow-direction', e.alpha + 'deg'))
+    document.querySelectorAll('.foo').forEach(x => x.style.setProperty('--shadow-direction', `${e.alpha}deg`))
 }, true);
